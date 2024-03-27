fffb74d0e51ab4973d65e9ab5b81194c27cbbaef
Matthew Araujo <matthewaraujo20@gmai.com>
2024-03-26 23:10:21 -0300

    feat: estrutuct programing done

--- fe/main.py
+++ fe/main.py
@@ -1,3 +1,4 @@
+import os
 from utils import liwc_analyze
 
 # Programação Estruturada (1.5pt):
@@ -7,11 +8,26 @@ from utils import liwc_analyze
 # ● Organize suas funções como quiser.
 # ● Organize seus dados como quiser.
 
-def stringReader(text): 
-  return text.split()
+def stringCleaner(text):
+  special_words = [33, 34, 35, 36, 37, 38, 42, 43, 45, 47, 58, 59, 60, 61, 62, 63, 64, 91, 92, 93, 94, 95, 96, 123, 124, 125, 126, 199]
+  cleaned_text = ""
+  for char in text:
+    if ord(char) == 199:
+      cleaned_text += "C"
+    if ord(char) not in special_words:
+      cleaned_text += char
+  return cleaned_text
 
-def tokenManipulator(tokens):
-  return [liwc_analyze(token) for token in tokens]
+
+def stringLower(text):
+  upper_case_ascii = [chr(i) for i in range(65, 91)]
+  lower_string = ""
+  for word in text:
+    if word in upper_case_ascii:
+      lower_string += chr(ord(word) + 32)
+    else:
+      lower_string += word
+  return lower_string
 
 def stringReader(text): 
   return text.split()
@@ -29,19 +45,30 @@ def liwcCounter(tokens):
         categories[category] = 1
   return categories
 
+def category_counter(tokens, category):
+  count = 0
+  for token in tokens:
+    if category in token:
+      count += 1
+  return count
 
-text = """
-Felicidade é viver na sua companhia
-Felicidade é estar contigo todo dia
-Felicidade é sentir o cheiro dessa flor
-Felicidade é saber que eu tenho seu amor
-"""
+def openFile(path_file):
+  return open(path_file, "r", encoding="utf8")
+  
 
-tokens = stringReader(text)
-tokens = tokenManipulator(tokens)
-categories = liwcCounter(tokens)
-swearCount = categories.get("swear", 0)
-anxCount = categories.get("anx", 0)
+def main(path_file):
+  text = openFile(path_file)
+  cleaned_text = stringCleaner(text)
+  lower_text = stringLower(cleaned_text)
+  tokens = stringReader(lower_text)
+  tokens = tokenManipulator(tokens)
+  posemoCount = (category_counter(tokens, "posemo") / len(tokens) ) * 100
+  negemoCount = (category_counter(tokens, "negemo") / len(tokens) ) * 100 
+  swearCount = category_counter(tokens, "swear")
+  anxCount = category_counter(tokens, "anx")
+  print(f"Saida : {len(tokens)} palavras,{swearCount} palavras ofensivas ,{anxCount} palavras de ansiedade, tom geralk positivo: {posemoCount:.2f}%, tom geral negativo: {negemoCount:.2f}%")
 
-print(f"Saida : {len(tokens)} palavras,{swearCount} palavras ofensivas ,{anxCount} palavras de ansiedade,")
 
+if __name__ == "__main__":
+  main("./texto2.txt")
+  
